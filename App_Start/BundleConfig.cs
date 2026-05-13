23e716098730a13de93b5a07e516f7dcd105f09c
Louis-Philippe Boucher <louisboucher2015@gmail.com>
2026-05-13 17:56:01 -0400

    Add files via upload

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Optimization;
+
+namespace Wikimedia
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                            "~/Scripts/validation.js",
+                            "~/Scripts/jquery-maskedinput.js",
+                            "~/Scripts/bootbox.js",
+                            "~/Scripts/selections.js",
+                            "~/Scripts/SiteScripts.js",
+                            "~/Scripts/pageManager.js",
+                            "~/Scripts/session.js",
+                            "~/Scripts/SiteNotificationsHandler.js",
+                            "~/Scripts/autoRefreshPanel.js",
+                            "~/Scripts/image-control.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                        "~/Content/_layout.css",
+                        "~/Content/Accounts.css",
+                        "~/Content/popup.css",
+                        "~/Content/Selections.css",
+                        "~/Content/site.css",
+                        "~/Content/menu.css",
+                        "~/Content/Students.css",
+                        "~/Content/Courses.css",
+                        "~/Content/Teachers.css",
+                        "~/Content/Icons.css",
+                        "~/Content/image-control.css",
+                        "~/Content/jqui-custom-datepicker.css"));
+        }
+    }
+}
